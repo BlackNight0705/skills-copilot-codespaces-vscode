635fcef1b43374eb8013ed9b1f36ed5876e6572d
BlackNight0705 <abdelgamer0001@gmail.com>
2024-06-01 16:31:26 -0500

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+  var member = new Member("John Doe");
+  member.addSkill("programming");
+  member.addSkill("design");
+  member.addSkill("writing");
+  member.addSkill("marketing");
+  member.addSkill("management");
+  member.addSkill("teaching");
+  member.addSkill("cooking");
+  member.addSkill("gardening");
+  member.addSkill("photography");
+
+  return member;
+}
